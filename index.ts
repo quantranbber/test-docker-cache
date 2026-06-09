d47a95d15de218de9fec27735aaf0c045d1dac6e
quantranbber <quan.tranbb@gmail.com>
2026-06-10 00:24:43 +0700

    update

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-console.log("Hello Docker Cache Test1");
+console.log("Hello Docker Cache Test14");
 console.log("IMAGE_TAG =", process.env.IMAGE_TAG);
 console.log("Started at =", new Date().toISOString());
